bec5776989a6de304a2f9d7d2649ea48a6b0a3c5
Arzoo Shamim <166494063+arzooshamim13@users.noreply.github.com>
2024-08-28 22:52:08 +0500

    Add files via upload

---
+++ Math Expressions/script.js
@@ -0,0 +1,155 @@
+// console.log('hello world');
+
+// question no 1
+// var num1 = 5;
+// var num2 = 4;
+// var total = num1 + num2;
+// document.write('Sum Of ' + num1 + ' And ' + num2 + ' Is ' + total);
+// question no 1
+
+// question no 2
+
+// Multiplication
+// var multiply = 5;
+// var multiply2 = 4;
+// var total = multiply * multiply2;
+// document.write('Multiply Of  ' + multiply + ' And ' + multiply2 + ' Is  ' + total);
+
+// Subtraction
+
+// var mainace = 5;
+// var mainace2 = 4;
+// var total = mainace - mainace2;
+// document.write('Subtraction Of  ' + mainace + ' And ' + mainace2 + ' Is  ' + total);
+
+//  Division
+
+// var  division = 5;
+// var  division2 = 4;
+// var total =  division /  division2;
+// document.write('Division Of  ' +  division + ' And ' +  division2 + ' Is  ' + total);
+
+// modulus
+
+// var  modulus = 5;
+// var  modulus2 = 4;
+// var total =  modulus %  modulus2;
+// document.write('Modulus Of  ' +  modulus + ' And ' +  modulus2 + ' Is  ' + total);
+// question no 2
+
+// question no 3
+// var no = undefined;
+// document.write('Value after variable declaration is '.toLocaleUpperCase()+ no)
+// var initial = 5;
+// initial++;
+// document.write('Initial Value Is '+ initial);
+// document.write('Value after increment is ',initial);
+// var add = initial + 7;
+// document.write('after addition is '+add);
+
+// add--;
+// document.write('Value after decrement is '+add);
+// var divid = 3 / add ;
+// document.write('The remainder is ' + divid );
+
+// question no 3
+
+
+// question no 4
+// var buying = 5;
+// var ticket = 600; 
+// var total = buying * ticket;
+// document.write('Total Cost To Buy '+ buying + ' TO A Movies Is '+total);
+// question no 4
+
+
+// question no 5
+
+// var total = 2*1;
+// console.log(total);
+// var total = 2*2;
+// console.log(total);
+// var total = 2*3;
+// console.log(total);
+// var total = 2*4;
+// console.log(total);
+// var total = 2*5;
+// console.log(total);
+// var total = 2*6;
+// console.log(total);
+// var total = 2*7;
+// console.log(total);
+// var total = 2*8;
+// console.log(total);
+// var total = 2*9;
+// console.log(total);
+// var total = 2*10;
+// console.log(total);
+
+// question no 5
+
+
+// question no 6
+// document.write('25 C Is 77 F');
+// document.write('70 F is 21.111111111111 C');
+// question no 6
+
+
+// question no 7
+// var num1 = 3*650;
+// var num2 = 1*3;
+// var num3 = 2*100;
+// var num4 = 2*7;
+// var num5 = 100;
+// var total = num1 + num2 + num3 + num4 + num5;
+// console.log(total); 
+// question no 7
+
+
+// question no 8
+// var totalMarks = 980;
+// var obident = 804;
+// var total = totalMarks % obident;
+// console.log(total);
+// question no 8
+
+// question no 9
+// var usd = 10;
+// var pak = 104.28;
+// var total = usd * pak;
+// console.log(total);
+// var saudi = 25;
+// var pakis =  28;
+// var totala = saudi * pakis;
+// console.log(totala);
+// console.log(total + totala);
+// question no 9
+
+// question no 10
+// var add = 5;
+// var multiply = 10;
+// var divide = 2;
+// console.log(add * multiply / divide);
+// question no 10
+
+
+// question no 11
+// var current = 2024;
+// var old = 2011;
+// var total = current % old;
+// console.log(total);
+// question no 11
+
+// question no 12
+// var circul  = 20;
+// var redius = 125.67999999999990;
+// console .log(redius);
+// question no 12
+
+var totla = 65 *3;
+console.log(totla)
+// question no 13
+var snack = 'choclate chip'
+var age = 14;
+var maximumAge  = 30;
+// question no 13
---
+++ Variable Names/script.js
@@ -0,0 +1,30 @@
+console.log('hello world');
+
+// question no 1
+// var name = 'Arzoo';
+// let name = 'Arzoo';
+// const name = 'Arzoo';
+// question no 1
+
+// question no 2
+
+// legal names
+// var name = 'Arzoo';
+// var fatherName = 'Muhammmad Shamim Uddin';
+// var email = 'nabeelqadri021@gmail.com';
+// var age = 14;
+// var githubUserName = 'arzooshamim13';
+
+// illegal names
+
+// var 1num = 1;
+// var var = 'what is your name';
+// var hello-world = 'hello world';
+// var @email  = 'hello@gmail.com';
+// var 021 = 12345;
+
+// question no 2
+
+
+
+
