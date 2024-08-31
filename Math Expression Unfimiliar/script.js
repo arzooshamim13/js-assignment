0f7e73a6db22efd1c29d6679d82a443624e9942b
Arzoo Shamim <166494063+arzooshamim13@users.noreply.github.com>
2024-08-31 06:24:46 +0500

    Add files via upload

---
+++ Math Expression Unfimiliar/script.js
@@ -0,0 +1,76 @@
+console.log('hello world');
+
+// question no 1
+ 
+// var orignalValue = 10;
+// ++orignalValue;
+// orignalValue++;
+// --orignalValue;
+// orignalValue--;
+// 
+// console.log(orignalValue);
+
+// question no 1
+
+// question no 2
+
+// var a = 2, b = 1;
+// console.log(result);
+// var result = --a - --b + ++b + b--;
+// answer is 3
+
+// question no 2
+
+
+// question no 3
+
+// var user = +prompt('Enter Your User Name');
+// var greet = +prompt('','Hello I Can Help You ?' + prompt.value);
+
+// question no 3
+
+// question no 4
+
+// var num = 1;
+// var table = +prompt('Table Number');
+// for(var i = 1; i < 10 ; i ++){
+//   i = ` ${table} x ${i} = ${table * i}`;
+//   console.log(i) ;   
+// }
+
+// question no 4
+// var message = +prompt('Enter Subject')
+// var subject1 = 'english';
+// var subject2 = 'Math';
+// var subject3 = 'Physics';
+
+
+// if(subject1 > message){
+//  console.log('Chalao')
+// }
+
+
+// document.getElementById('English');
+// document.getElementById('Urdu');
+// document.getElementById('Math');
+// document.getElementById('Pst');
+// var message = +prompt('Enter Your Subject')
+// var inpput = prompt.value;
+ // document.write(inpput.value) ;
+
+
+// question no 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
