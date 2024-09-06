ae3b35fc64118a9c68ec47ac60497c3e76deb93b
Arzoo Shamim <166494063+arzooshamim13@users.noreply.github.com>
2024-09-06 06:45:53 +0500

    Add files via upload

---
+++ If Else And Else If Statment/script.js
@@ -0,0 +1,31 @@
+console.log('Hello world');
+
+
+// question no 1
+
+// var alpha = 'Abacdh';
+// var number = 1835;
+// var result = alpha+number+alpha +number;
+// console.log(result);
+
+// question no 1
+
+
+// question no 2
+
+var number = 1+-2;
+console.log(number)
+
+// question no 2
+
+
+// question no 3
+// question no 3
+
+
+// question no 4
+// question no 4
+
+
+// question no 5
+// question no 5
